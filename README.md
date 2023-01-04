ad8ff98b0b196b9709fedaf19cc411db73173472
aquagamer <j00968361>
2023-01-05 07:42:48 +0800

    hw

--- README.md
+++ README.md
@@ -3,5 +3,11 @@
 1. add docker
 2. add server
 3. change url in frontend
+
+
+
+import path from "path"
+import express from "express"
+
 4. change backend package to daploy  ```"deploy": "NODE_ENV=production babel-node src/indexjs"```
 5. add three line on package
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import { createServer } from 'node:http'
 import { createSchema, createYoga } from 'graphql-yoga'
 import {UserModel, MapModel} from './models'
 import path from "path"
+import express from "express"
 
 import Query from './resolvers/Query';
 import Mutation from './resolvers/Mutation';
