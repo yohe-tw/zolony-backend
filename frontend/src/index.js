fb75af52c1da3fda014105955b045a3a375c7fbb
aquagamer <j00968361>
2023-01-05 11:11:08 +0800

    he

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,7 @@ import { HookProvider } from './hook/usehook';
 
 const PORT = process.env.PORT || 4000;
 const client = new ApolloClient({ 
-  uri: PORT,
+  uri: '/',
   cache: new InMemoryCache(),
 });
 
